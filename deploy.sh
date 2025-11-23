bb9af827933e5d0ccc6c7a9f6ebba00373d3f5bd
Madam-JungLe <jung.simon.2002@gmail.com>
2025-11-23 14:27:08 +0100

    Deploying new dist

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-git pull
-git rm -r dist
-git commit -m 'Removing old dist'
-npm run build
-git add dist 
-git commit -m 'Deploying new dist'
-git push 
-git push -d origin github-pages
-git subtree push --prefix dist origin github-pages

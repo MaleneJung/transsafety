8145dc6b625e9152079f9b87915a4be7e704a356
Madam-JungLe <jung.simon.2002@gmail.com>
2025-12-23 12:34:16 +0100

    Added encrypt and decrypt method content aswell as utilities

--- src/utilities/encryption.ts
+++ src/utilities/encryption.ts
@@ -1,13 +1,56 @@
 export const ENCRYPTION = {
   sha256Hex: async (str: string) => {
-    const msgUint8 = new TextEncoder().encode(str)
-    const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgUint8)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
-    return hashHex
+    const bytes = new TextEncoder().encode(str)
+    const hashBuffer = await window.crypto.subtle.digest('SHA-256', bytes)
+    const hashBytes = new Uint8Array(hashBuffer)
+    return ENCRYPTION.bytesToHex(hashBytes)
+  },
+  cycleBytes: (bytes: Uint8Array, key: Uint8Array, reverse: boolean): Uint8Array => {
+    const output: number[] = []
+    for (let i = 0; i < bytes.length; i++) {
+      const cycle = key[i % key.length]!
+      let byte = reverse ? bytes[i]! - cycle : bytes[i]! + cycle
+      while (byte > 255) byte -= 256
+      while (byte < 0) byte += 256
+      output.push(byte)
+    }
+    return new Uint8Array(output)
+  },
+  bytesToHex: (bytes: Uint8Array): string => {
+    return Array.from(bytes)
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('')
+  },
+  hexToBytes: (hex: string): Uint8Array => {
+    return Uint8Array.from(hex.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16)))
   },
   iterativeHashingHex: {
-    encrypt: async (str: string, key: string): Promise<string> => '',
-    decrypt: async (str: string, key: string): Promise<string> => '',
+    generateIterativeHashingKey: async (len: number, key: string): Promise<Uint8Array> => {
+      let iterativeKey = ''
+      let iterativeIndex = 0
+      while (iterativeKey.length < len) {
+        iterativeKey += await ENCRYPTION.sha256Hex(iterativeIndex + key)
+        iterativeIndex++
+      }
+      return new TextEncoder().encode(iterativeKey)
+    },
+    encrypt: async (str: string, key: string): Promise<string> => {
+      const bytes = new TextEncoder().encode(str)
+      const iterativeKey = await ENCRYPTION.iterativeHashingHex.generateIterativeHashingKey(
+        bytes.length,
+        key,
+      )
+      const cycledBytes = ENCRYPTION.cycleBytes(bytes, iterativeKey, false)
+      return ENCRYPTION.bytesToHex(cycledBytes)
+    },
+    decrypt: async (str: string, key: string): Promise<string> => {
+      const bytes = ENCRYPTION.hexToBytes(str)
+      const iterativeKey = await ENCRYPTION.iterativeHashingHex.generateIterativeHashingKey(
+        bytes.length,
+        key,
+      )
+      const cycledBytes = ENCRYPTION.cycleBytes(bytes, iterativeKey, true)
+      return new TextDecoder().decode(cycledBytes)
+    },
   },
 }

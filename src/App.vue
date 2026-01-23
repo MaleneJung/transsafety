6a27136e5652b94b1f2d7e2b60c56116976068a2
Malene Jung <jung.malene.2002@gmail.com>
2026-01-23 11:48:04 +0100

    Added basic app design & routeState

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Route, ROUTER, routeState } from './core/router'
+import SafeListView from './views/SafeListView.vue'
+import SafeCreateView from './views/SafeCreateView.vue'
+import SafeUnlockedView from './views/SafeUnlockedView.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="wrapper">
+    <div id="header">
+      <h1 @click="ROUTER.go(Route.List)">Transsafety</h1>
+    </div>
+    <div id="current-view">
+      <SafeListView v-if="routeState === Route.List" />
+      <SafeCreateView v-else-if="routeState === Route.Create" />
+      <SafeUnlockedView v-else-if="routeState === Route.Unlocked" />
+      <p v-else>ERROR: Unknown Route</p>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#wrapper {
+  background-color: #222228;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+}
+
+#header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 3rem;
+  padding: 1rem;
+  background: linear-gradient(135deg, #00bbff88, #ff00bb88);
+  border-bottom: #111114 1px solid;
+  backdrop-filter: blur(4px);
+}
+
+#header h1 {
+  color: #bbbbbb;
+  cursor: pointer;
+}
+
+#header h1:hover {
+  color: #ffffff;
+}
+
+#current-view {
+  margin-top: 5rem;
+}
+</style>
---
+++ src/views/SafeUnlockedView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Route, ROUTER } from '@/core/router'
+</script>
+
+<template>
+  <button @click="ROUTER.go(Route.List)">Unlocked</button>
+</template>
